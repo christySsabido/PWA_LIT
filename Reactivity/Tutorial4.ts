2e3b630fe91f88dd832d9284e02d7a1443d91924
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 17:42:39 -0600

    reeac3

--- Reactivity/Tutorial4.ts
+++ Reactivity/Tutorial4.ts
@@ -1,51 +1,36 @@
-import {LitElement, html, css, PropertyValues} from 'lit';
-import {customElement, property, state} from 'lit/decorators.js';
-import {classMap} from 'lit/directives/class-map.js';
-import {animate} from '@lit-labs/motion';
+import {LitElement, html, PropertyValues} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-  @property({type: Boolean}) big = false;
-  @property({type: Number}) duration = 500;
-  @state() _renderCount = 0;
+  @property() forward = '';
+  @property() backward = '';
 
-  static styles = css`
-    .bar {
-      background: red;
-      height: 2em;
-      width: 10vw;
+  willUpdate(changedProperties: PropertyValues<this>) {
+    if (changedProperties.has('forward')) {
+      this.backward = this.forward.split('').reverse().join('');
     }
 
-    .big {
-      width: 50vw;
+    if (changedProperties.has('backward')) {
+      this.forward = this.backward.split('').reverse().join('');
     }
-  `;
-
-  setDuration(e: Event) {
-    const v = (e.target as HTMLSelectElement).value;
-    this.duration = Number.parseInt(v);
   }
 
-  shouldUpdate(changedProperties: PropertyValues<this>): boolean {
-    return !(changedProperties.size === 1 && changedProperties.has('duration'));
+  onInput(e: Event) {
+    const inputEl = e.target as HTMLInputElement;
+    if (inputEl.id === 'forward') {
+      this.forward = inputEl.value;
+    } else {
+      this.backward = inputEl.value;
+    }
   }
 
   render() {
-    this._renderCount++;
-    const keyframeOptions = { duration: this.duration };
-
     return html`
-      <p>
-        <button @click=${() => (this.big = !this.big)}>Animate</button>
-      </p>
-      <p>
-        <label>Speed <select @change=${this.setDuration}>
-          <option value="250" selected>Fast</option>
-          <option value="1500">Slow</option>
-        </select></label>
-        Render count: ${this._renderCount}
-      </p>
-      <p class="bar ${classMap({big: this.big})}" ${animate({keyframeOptions})}></p>
+      <label>Forward: <input id="forward" @input=${this.onInput} .value=${this.forward}></label>
+      <label>Backward: <input id="backward" @input=${this.onInput} .value=${this.backward}></label>
+      <div>Forward text: ${this.forward}</div>
+      <div>Backward text: ${this.backward}</div>
     `;
   }
 }
