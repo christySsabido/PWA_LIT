67046703b237655f05a852d0f3300d3571677c6c
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 14:47:03 -0600

    intro2

--- Intro tO LIT/my-element.ts
+++ Intro tO LIT/my-element.ts
@@ -1,14 +1,22 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('name-tag')
+export class NameTag extends LitElement {
   @property()
-  message: string = 'Hello again.';
+  name: string = 'Your name here';
 
   render() {
+    // TODO: Add declarative event listener to input.
     return html`
-      <p>${this.message}</p>
+      <p>Hello, ${this.name}</p>
+      <input @input=${this.changeName} placeholder="Enter your name">
     `;
   }
+  chengeName(event: Event){
+    const input = event.target as HTMLInputElement;
+    this.name = input.value;
+  }
+
+  // TODO: Add event handler method.
 }
