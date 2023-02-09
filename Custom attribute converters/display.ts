4a0bb3961742e4cbf8dcde6b34a276eac69b26f6
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 17:58:28 -0600

    display1

--- Custom attribute converters/display.ts
+++ Custom attribute converters/display.ts
@@ -1,11 +1,13 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {dateConverter} from './date-converter.js';
 
 @customElement('date-display')
 export class DateDisplay extends LitElement {
-  @property({converter: dateConverter})
+  @property({attribute: false})
   date = new Date();
+
+  @property({type: String, attribute: 'date-str'})
+  dateStr = '';
 
   render() {
     const locale = 'en-US';
