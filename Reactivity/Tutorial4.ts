34fae2edb2685fec32eaf6827d270f0a1afca0d2
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 17:50:56 -0600

    reeac5

--- Reactivity/Tutorial4.ts
+++ Reactivity/Tutorial4.ts
@@ -1,8 +1,8 @@
 import {LitElement, html, css, PropertyValues} from 'lit';
 import {customElement, state, query} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('bye-element')
+export class ByeElement extends LitElement {
   static styles = css`
     :host {
       display: block;
@@ -24,18 +24,18 @@ export class MyElement extends LitElement {
     return html`
       <button @click=${() => this._showMessage = !this._showMessage}>Click me</button>
       <div id="message" ?hidden=${!this._showMessage}>
-        TADA
+        That's all folks!
       </div>
     `;
   }
 
   protected updated(changedProperties: PropertyValues<this>): void {
     if (changedProperties.has('_showMessage')) {
-      const final = this._message.getBoundingClientRect().width;
-      const starting = 0 - final;
+      const rect = this._message.getBoundingClientRect();
+      const startingX = 0 - rect.width;
       this._message.animate([
-        { transform: `translateX(${starting}px)` },
-        { transform: `translateX(0)` }
+        { transform: `translateX(${startingX}px) scale(0.1)` },
+        { transform: `translateX(0) translateY(0) scale(1)` }
       ], {
         duration: 500,
         easing: 'ease-out',
@@ -43,4 +43,5 @@ export class MyElement extends LitElement {
     }
   }
 }
+
 
