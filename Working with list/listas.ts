2e72163d445853a06f2a4293ec22aab30568c699
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 15:59:29 -0600

    lista1

--- Working with list/listas.ts
+++ Working with list/listas.ts
@@ -1,18 +1,20 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  items = new Set(['Apple', 'Banana', 'Grape', 'Orange', 'Lime'])
+  names = ['Chandler', 'Phoebe', 'Joey', 'Monica', 'Rachel', 'Ross'];
 
   render() {
     return html`
-      <p>My unique fruits</p>
+      <p>A list of names that include the letter "e"</p>
       <ul>
-        ${map(this.items, (item) => html`<li>${item}</li>`)}
+      ${this.names
+        .filter((name) => name.match(/e/i))
+        .map((name) => html`<li>${name}</li>`)}
       </ul>
     `;
   }
 }
+
