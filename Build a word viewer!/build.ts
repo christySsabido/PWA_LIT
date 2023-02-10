c5468a07f5da32e157dcf292c36aabd96c4a00f0
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 23:16:33 -0600

    viewer4

--- Build a word viewer!/build.ts
+++ Build a word viewer!/build.ts
@@ -6,9 +6,26 @@ class WordViewer extends LitElement {
   @state() private idx = 0;
   @property() words = 'initial value';
 
+  private intervalTimer?: number;
+  
+  connectedCallback() {
+    super.connectedCallback();
+    this.intervalTimer = setInterval(this.tickToNextWord, 1000);
+  }
+  
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.intervalTimer);
+    this.intervalTimer = undefined;
+  }
+
   render() {
     const splitWords = this.words.split('.');
     const word = splitWords[this.idx % splitWords.length];
     return html`<pre>${word}</pre>`;
   }
+
+  tickToNextWord = () => { this.idx += 1; };
 }
+
+
