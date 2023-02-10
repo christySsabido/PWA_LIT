f6780a99dce6dcd0b0c68d89044510e931bc67c4
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-09 23:10:42 -0600

    viewer3

--- Build a word viewer!/build.ts
+++ Build a word viewer!/build.ts
@@ -1,12 +1,14 @@
 import { html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('word-viewer')
 class WordViewer extends LitElement {
+  @state() private idx = 0;
   @property() words = 'initial value';
 
   render() {
-    return html`<pre>${this.words}</pre>`
+    const splitWords = this.words.split('.');
+    const word = splitWords[this.idx % splitWords.length];
+    return html`<pre>${word}</pre>`;
   }
 }
-
